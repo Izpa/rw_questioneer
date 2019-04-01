a138297caec3c3a87a8630221c1858d543c91c22
Артем Марков <a.markov@realweb.ru>
2019-04-01 16:01:55 +0300

    fix telegram handler for webhook

--- src/rw_questioneer/bot.clj
+++ src/rw_questioneer/bot.clj
@@ -12,18 +12,5 @@
 (api/set-webhook s/telegram-token webhook-url)
 
 (h/defhandler handler
-
-  (h/command-fn "start"
-                (fn [{{id :id :as chat} :chat}]
-                  (println "Bot joined new chat: " chat)
-                  (api/send-text s/telegram-token id "Welcome to rw_questioneer!")))
-
-  (h/command-fn "help"
-                (fn [{{id :id :as chat} :chat}]
-                  (println "Help was requested in " chat)
-                  (api/send-text s/telegram-token id "Help is on the way")))
-
-  (h/message-fn
-   (fn [{{id :id} :chat :as message}]
-     (println "Intercepted message: " message)
-     (api/send-text s/telegram-token id "I don't do a whole lot ... yet."))))
+  (h/command "help" {{id :id} :chat}
+           (api/send-text token id "Help is on the way")))
