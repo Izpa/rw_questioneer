e4c64d9a742a2bdd53b66bb576a00d9099765048
Артем Марков <a.markov@realweb.ru>
2019-04-01 12:27:18 +0300

    fixes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JVM_OPTS -cp target/rw_questioneer.jar 
+web: java $JVM_OPTS -cp target/rw_questioneer.jar clojure.main -m rw_questioneer.core
--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
 
   :plugins [[lein-environ "1.1.0"]]
 
-  :main rw-questioneer.core
+  :main rw_questioneer.core
   :target-path "target/%s"
   :uberjar-name "rw_questioneer.jar"
   :profiles {:uberjar {:aot :all}})
