265b4c057dae042e9fc13d02bb7e6bfac404c4a2
Артем Марков <a.markov@realweb.ru>
2019-04-01 17:16:45 +0300

    fix

--- src/rw_questioneer/web.clj
+++ src/rw_questioneer/web.clj
@@ -11,12 +11,17 @@
    :headers {"Content-Type" "text/plain"}
    :body "Hello from Heroku"})
 
+(defn telegram-handler [request]
+  ;;(map bot/handler updates))
+  (println (str (:body request)))
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body ""})
+
 (defroutes app
   (GET "/" []
        (splash))
-  (POST s/telegram-handler-uri {body :body}
-        ;;(map bot/handler updates))
-        (println (str body))
-        (splash))
+  (POST s/telegram-handler-uri request (telegram-handler request))
+ 
   (ANY "*" []
        (route/not-found (slurp (io/resource "404.html")))))
