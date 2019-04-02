566436ebcdc42897f47af71061ac6346c5586d56
Артем Марков <a.markov@realweb.ru>
2019-04-02 04:41:25 +0300

    change help message and set-webhook method

--- src/rw_questioneer/bot.clj
+++ src/rw_questioneer/bot.clj
@@ -3,14 +3,14 @@
             [morse.api :as api]
             [rw-questioneer.settings :as s]))
 
-
-(def webhook-url (str "https://" s/domain s/telegram-handler-uri))
-
-(api/set-webhook s/telegram-token webhook-url)
+(defn set-webhook []
+  (let [webhook-url (str "https://" s/domain s/telegram-handler-uri)]
+    (api/set-webhook s/telegram-token webhook-url)))
 
 (h/defhandler handler
-  (h/command "help" message
-             (api/send-text s/telegram-token (:id (:chat message)) message))
+  (h/command "help" {{chat-id :id} :chat}
+             (api/send-text s/telegram-token chat-id
+                            "Просто напишите ваш вопрос, он будет отправлен спикеру нашей интеллектуальной системой \"О\""))
   (h/command "id" {{user-id :id} :from {chat-id :id} :chat}
              (api/send-text s/telegram-token chat-id user-id))
   (h/message {{first-name :first_name last-name :last_name user-name :username} :from text :text}
